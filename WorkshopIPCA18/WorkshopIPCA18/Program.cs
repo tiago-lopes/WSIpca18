3432da613dee9e15bc4217f823c4c993619087a5
Tiago Lopes <tiago.lopes@xpand-it.com>
2018-10-09 21:11:50 +0100

    -testing

--- WorkshopIPCA18/WorkshopIPCA18/Program.cs
+++ WorkshopIPCA18/WorkshopIPCA18/Program.cs
@@ -7,6 +7,7 @@ namespace WorkshopIPCA18
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
