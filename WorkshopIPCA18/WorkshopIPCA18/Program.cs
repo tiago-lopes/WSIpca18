c3901a9481e3f7d49a3e3b9f11f7ac5916994484
Tiago Lopes <h0l0cau5to@gmail.com>
2018-10-09 21:17:51 +0100

    - testing x2

--- WorkshopIPCA18/WorkshopIPCA18/Program.cs
+++ WorkshopIPCA18/WorkshopIPCA18/Program.cs
@@ -8,6 +8,7 @@ namespace WorkshopIPCA18
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
